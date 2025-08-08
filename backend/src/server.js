d0b9dd13aee9954a4a57196a2b547463cb7820b2
Wambua <ley9@cdc.gov>
2025-08-08 14:13:26 -0400

    added .env

---
+++ backend/src/server.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import 'dotenv/config';
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.listen(3000, () => {
+  console.log('Server is running on port:', PORT);
+});
