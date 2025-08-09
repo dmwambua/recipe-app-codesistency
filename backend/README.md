7114cce7b751e522b11f6b0009ac39c2f321fdf6
dmwambua <dickmuoki@gmail.com>
2025-08-08 23:48:54 -0400

    completed adding config and env files

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,5 @@
+https://www.youtube.com/watch?v=fLIl6jypzkI&t=716s
+Minute 15:18
 npm run dev to RUN
+tree To see the directory tree. This is a function of Homebrew use brew install tree
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import 'dotenv/config';
+import { ENV } from './config/env.js';
 const app = express();
-const PORT = process.env.PORT || 3000;
-app.listen(3000, () => {
+const PORT = ENV.PORT || 3000;
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: true, message: "API is healthy" });
+});
+app.listen(PORT, () => {
   console.log('Server is running on port:', PORT);
 });
