b77842d2938659340c06d05efa592f10fbad8770
Wambua <ley9@cdc.gov>
2025-08-11 19:51:40 -0400

    moved drizzle,config.js into backend from src

--- backend/drizzle.config.js
+++ backend/drizzle.config.js
@@ -1,5 +1,5 @@
 // backend/src/drizzle.config.js
-import { ENV } from "./config/env.js";
+import { ENV } from "./src/config/env.js";
 export default {
     schema: "./src/db/schema.js",//specifies the schema file
     out: "./src/db/migrations", //specifies the output directory
