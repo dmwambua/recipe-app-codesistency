77d0f206591dd364bca122fcef791f6d70cba378
dmwambua <dickmuoki@gmail.com>
2025-08-14 22:44:26 -0400

    Edited Index.tsx to include a stylesheet

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
-
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+import UzaNunuaLogo from "../assets/images/UZA-Nunua-Logo-2.png";
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome to the DW recipe app!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to the DW recipe app!</Text>
+      <Image
+        source={UzaNunuaLogo}
+        style={{
+          width: 100,
+          height: 100
+        }}
+      />
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: { color: "blue", fontSize: 20, fontWeight: "bold" }
+});
