e42ef78c38b77b3515179b8597e5560c91315d1d
Wambua <ley9@cdc.gov>
2025-08-21 13:56:10 -0400

    Corrected an error occuring due to not impoting COLORS in SafeScreen

--- mobile/components/SafeScreen.jsx
+++ mobile/components/SafeScreen.jsx
@@ -1,6 +1,6 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-
+import { COLORS } from "../constants/colors";
 
 const SafeScreen = ({ children }) => {
     const insets = useSafeAreaInsets()
